0679d2762b0c4ba7d6adf467e77f2ab9481783cc
Diego A <damaya@aplyca.com>
2014-04-07 01:30:07 -0500

    agregar estilos a nueva aplicación de eventos

--- src/Yoda/EventBundle/Controller/DefaultController.php
+++ src/Yoda/EventBundle/Controller/DefaultController.php
@@ -3,14 +3,31 @@
 namespace Yoda\EventBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Yoda\EventBundle\Entity\Event;
 
 class DefaultController extends Controller
 {
     public function indexAction($name,$count)
     {
-        return $this->render(
+        $em = $this->getDoctrine()->getManager();
+        $nem = $this->get('doctrine')->getManager();
+		$repo = $em->getRepository('YodaEventBundle:Event');
+
+		$newEvent = new Event();
+		$newEvent->setName($name);
+		$newEvent->setLocation($count);
+		$newEvent->setTime(new \DateTime('tomorrow noon'));
+		$newEvent->setDetails('Ha! Darth HATES surprises!!!!');
+		$nem->persist($newEvent);
+		$nem->flush();
+
+		$event = $repo->findOneBy(array(
+		'name' => "Yoda surprise birthday party",
+		));
+		return $this->render(
             'YodaEventBundle:Default:index.html.twig',
-            array('name' => $name,'count' =>$count)
+            array('name' => $name,'count' =>$count,'event'=> $event)
         );
     }
 }
+
